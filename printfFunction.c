e6ef12f0071aac293bd048c30ccfc717087d60b5
amepila <ie704453@iteso.mx>
2018-01-28 22:20:31 -0600

    Functional code

--- printfFunction.c
+++ printfFunction.c
@@ -3,7 +3,8 @@
 #include <stdint.h>
 
 #define va_start(v,l)	__builtin_va_start(v,l)
-#define va_end(v)	__builtin_va_end(v)
+#define va_end(v)	    __builtin_va_end(v)
+#define va_arg(v,l)	    __builtin_va_arg(v,l)
 
 typedef __gnuc_va_list va_list;
 
@@ -13,9 +14,9 @@ int main()
 {
     uint32_t total;
 
-    total = sum(4, 1,2,3,4);
+    total = sum(4, 1, 2, 3, 4);
 
-    printf("Result = %d", total);
+    printf("\nResult = %d\n", total);
 
     return 0;
 
@@ -23,16 +24,21 @@ int main()
 
 uint32_t sum(uint8_t size_of_list, ...)
 {
-    uint32_t counter;
+    uint32_t counter_size;
     uint32_t result;
+    uint32_t element_list;
 
     va_list arg;
 
     result = 0;
 
-    for(counter = 0; counter < size_of_list; counter++)
+    va_start(arg, size_of_list);
+
+    for(counter_size = 0; counter_size < size_of_list; counter_size++)
     {
-        result += printf("%*s",arg,"");
+        element_list = va_arg(arg, uint32_t);
+
+        result += printf("%*s",element_list,"");
 
     }
 
