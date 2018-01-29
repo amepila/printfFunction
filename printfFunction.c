367f13e478c7238bec9cd4ddec69f40a87d1f3a1
amepila <ie704453@iteso.mx>
2018-01-28 20:51:21 -0600

    Macros added

--- printfFunction.c
+++ printfFunction.c
@@ -2,29 +2,42 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t sum(uint32_t numA, uint32_t numB);
+#define va_start(v,l)	__builtin_va_start(v,l)
+#define va_end(v)	__builtin_va_end(v)
+
+typedef __gnuc_va_list va_list;
+
+uint32_t sum(uint8_t size_of_list, ...);
 
 int main()
 {
-    uint32_t argA;
-    uint32_t argB;
     uint32_t total;
 
-    printf("Operand 1: \n");
-    scanf("%d",&argA);
+    total = sum(4, 1,2,3,4);
 
-    printf("Operand 2: \n");
-    scanf("%d",&argB);
-
-    total = sum(argA, argB);
     printf("Result = %d", total);
 
     return 0;
 
 }
 
-uint32_t sum(uint32_t numA, uint32_t numB)
+uint32_t sum(uint8_t size_of_list, ...)
 {
-    return (printf("%*s%*s", numA, "",numB, ""));
+    uint32_t counter;
+    uint32_t result;
+
+    va_list arg;
+
+    result = 0;
+
+    for(counter = 0; counter < size_of_list; counter++)
+    {
+        result += printf("%*s",arg,"");
+
+    }
+
+    va_end(arg);
+
+    return (result);
 
 }
